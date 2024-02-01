3b4ffc2fdf864e4521f94f8016fda21dd640538c
jinchen62 <jinchenye62@gmail.com>
2024-02-01 13:33:15 -0800

    fix bert

--- test_model.sh
+++ test_model.sh
@@ -11,11 +11,18 @@ elif [[ $DEVICE =~ "rocm" ]]; then
   TARGET_FLAGS=("--iree-hal-target-backends=rocm" "--iree-rocm-target-chip=gfx940")
 fi
 
+rm results.txt
+
 list="./model_list.txt"
 while IFS= read -r HF_ID
 do
   echo "$HF_ID"
   NAME=${HF_ID//['-''/']/'_'}
+
+  INPUTS=("--input=1x1xi64")
+  if [[ $HF_ID =~ "bert" ]]; then
+    INPUTS=("--input=1x1xi64" "--input=1x1xi64")
+  fi
 
   python python/turbine_models/model_builder.py "${HF_ID}" > tmp.txt
   if [[ ! "$(cat tmp.txt)" =~ "done" ]]; then
@@ -29,10 +36,12 @@ do
     continue
   fi
 
-  iree-benchmark-module --module="${NAME}".vmfb --device="${DEVICE}" --function=run --input=1x1xi64 > tmp.txt
+  iree-benchmark-module --module="${NAME}".vmfb --device="${DEVICE}" --function=run "${INPUTS[@]}" > tmp.txt
   if [[ "$(cat tmp.txt)" =~ "items_per_second" ]]; then
     echo "${HF_ID} Pass" >> results.txt
   else
     echo "${HF_ID} Fail" >> results.txt
   fi
 done < "$list"
+
+rm tmp.txt
